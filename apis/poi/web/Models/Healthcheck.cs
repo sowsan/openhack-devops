e64a99daa88332bb342a8a55658d3183b16f9db6
sowmyan soman <sowmyans@outlook.com>
2020-09-02 14:22:52 -0400

    Update Healthcheck.cs

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Awesomelly Healthy";
+            Status = "I am kind of Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
